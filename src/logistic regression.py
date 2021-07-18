3339b8639229ed47d30a27408109c39f9cbf9f62
Bakar31 <abubakar1808031@gmail.com>
2021-07-19 00:49:44 +0600

    feature selection and knn

--- src/logistic regression.py
+++ src/logistic regression.py
@@ -2,6 +2,11 @@ from preprocessing import *
 #from prep import *
 from sklearn.linear_model import LogisticRegression
 
+# model-1
 clf = LogisticRegression(max_iter = 1000, random_state = 31)
 clf.fit(x_train, y_train)
-print(clf.score(x_test, y_test))
+print(clf.score(x_test, y_test)) # Score -> 1.00
+
+from sklearn.metrics import classification_report
+y_preds = clf.predict(x_test)
+print(classification_report(y_test, y_preds))
