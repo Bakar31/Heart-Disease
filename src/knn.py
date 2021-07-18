8bd04bb47c4b7a90dc26e2047528b475882ae58d
Bakar31 <abubakar1808031@gmail.com>
2021-07-19 01:28:51 +0600

    knn best params

--- src/knn.py
+++ src/knn.py
@@ -2,9 +2,13 @@ from preprocessing import *
 from sklearn.neighbors import KNeighborsClassifier
 
 # Model-2
-knn_clf = KNeighborsClassifier()
+# Parameters taken from griid search best params.
+knn_clf = KNeighborsClassifier(algorithm = 'auto',
+                               leaf_size = 10,
+                               n_neighbors = 2,
+                               p = 2)
 knn_clf.fit(x_train, y_train)
-print(knn_clf.score(x_test, y_test)) # Score -> 0.945
+print(knn_clf.score(x_test, y_test)) # Score -> 0.969
 
 from sklearn.metrics import classification_report
 y_preds = knn_clf.predict(x_test)
