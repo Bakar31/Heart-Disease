559df0bc911a6e0a12a62f66e8b602b5ee36872f
Bakar31 <abubakar1808031@gmail.com>
2021-07-20 14:22:21 +0600

    random forest

--- src/random forest.py
+++ src/random forest.py
@@ -1,8 +1,10 @@
 from preprocessing import *
-
 from sklearn.ensemble import RandomForestClassifier
 
-rand_clf = RandomForestClassifier(n_estimators=1000, random_state = 35)
+rand_clf = RandomForestClassifier(random_state = 31)
 rand_clf.fit(x_train, y_train)
-ranf_score = rand_clf.score(x_test, y_test)
-ranf_score
+print(rand_clf.score(x_test, y_test)) # Score -> 1.00
+
+from sklearn.metrics import classification_report
+y_preds = rand_clf.predict(x_test)
+print(classification_report(y_test, y_preds))
