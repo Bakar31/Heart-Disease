4d9f54db7793bb041a59b9a5b7ab3f44fc9d4349
Bakar31 <abubakar1808031@gmail.com>
2021-07-20 14:23:47 +0600

    svc

--- src/svc.py
+++ src/svc.py
@@ -1,6 +1,7 @@
 from preprocessing import *
 from sklearn import svm
 
+# Model-3
 svc_clf = svm.SVC(random_state = 7)
 svc_clf.fit(x_train, y_train)
 print(svc_clf.score(x_test, y_test)) # Score -> 1.00
