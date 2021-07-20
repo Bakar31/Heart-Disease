6bc6c2a6b7998f6008ca9a14d7898f183bf727bb
Bakar31 <abubakar1808031@gmail.com>
2021-07-20 14:13:49 +0600

    svc classification report

--- src/svc.py
+++ src/svc.py
@@ -3,4 +3,8 @@ from preprocessing import *
 from sklearn import svm
 svc_clf = svm.SVC(random_state = 7)
 svc_clf.fit(x_train, y_train)
-print(svc_clf.score(x_test, y_test))
+print(svc_clf.score(x_test, y_test)) # Score -> 1.00
+
+from sklearn.metrics import classification_report
+y_preds = svc_clf.predict(x_test)
+print(classification_report(y_test, y_preds))
