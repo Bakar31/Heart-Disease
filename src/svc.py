48d4e39bd6bb741a72a380356a3a718e4eb4c2d7
Bakar31 <abubakar1808031@gmail.com>
2021-07-21 23:06:29 +0600

    just right model

--- src/svc.py
+++ src/svc.py
@@ -1,6 +1,7 @@
 from preprocessing import *
 from sklearn import svm
 
+# model -3
 svc_clf = svm.SVC(random_state = 7)
 svc_clf.fit(x_train, y_train)
 print(svc_clf.score(x_test, y_test)) # Score -> 1.00
@@ -9,6 +10,7 @@ from sklearn.metrics import classification_report
 y_preds = svc_clf.predict(x_test)
 print(classification_report(y_test, y_preds))
 
+# dev set
 devx = dev.drop('target', axis = 1)
 devy = dev['target']
 
